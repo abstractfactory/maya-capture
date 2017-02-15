08f8a9863ffe19d5cbf76fca37b89049804ee5eb
Roy Nieterau <roy_nieterau@hotmail.com>
2017-02-15 17:11:53 +0100

    Version bump (2.2.0)

--- capture.py
+++ capture.py
@@ -17,7 +17,7 @@ except ImportError:
     from PySide import QtGui
     QtWidgets = QtGui
 
-version_info = (2, 1, 1)
+version_info = (2, 2, 0)
 
 __version__ = "%s.%s.%s" % version_info
 __license__ = "MIT"
